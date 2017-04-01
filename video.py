b087df46238c52b34c5ee8ea96bf2a343abcd74e
Emils Matiss <emils.matiss@gmail.ca>
2017-04-01 15:07:24 -0400

    VR

--- video.py
+++ video.py
@@ -2,13 +2,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    contour = cv2.Canny(gray, 100, 200)
-    cv2.imshow('frame',contour)
+    #print(gray.shape)
+    gray = gray[0:720, 280:1000]
+    contour = cv2.Canny(gray, 80, 200)
+    
+    
+    combined = cv2.addWeighted(contour, 0.8, gray, 0.2, 0)
+    stream = np.concatenate((combined, combined), axis=1)
+    cv2.imshow('frame',stream)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
