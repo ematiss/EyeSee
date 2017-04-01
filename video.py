1a2fe2349ed2a9d1b0aad9c513be51aa9167f6c6
Emils Matiss <emils.matiss@gmail.ca>
2017-04-01 13:36:26 -0400

    added countours

--- video.py
+++ video.py
@@ -2,13 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
+    contour = cv2.Canny(gray, 100, 200)
+    cv2.imshow('frame',contour)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
