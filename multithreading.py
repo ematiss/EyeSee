a59e3c370b5335ef725b2917cf4e76934f3bccc3
Emils Matiss <emils.matiss@gmail.ca>
2017-04-01 22:59:54 -0400

    wip

--- multithreading.py
+++ multithreading.py
@@ -10,7 +10,7 @@ import time
 
 class FLANN(Process):
 
-    def __init__(self,frame_queue, output_queue, descriptor, height, width):
+    def __init__(self,frame_queue, output_queue, descriptor, trainKP, height, width):
         Process.__init__(self)
         self.frame_queue = frame_queue
         self.output_queue = output_queue
@@ -18,6 +18,7 @@ class FLANN(Process):
         self.width = width
         self.stop = False
         self.descriptor = descriptor
+        self.trainKP = trainKP
 
 
     def get_frame(self):
@@ -42,7 +43,7 @@ class FLANN(Process):
             tp=[]
             qp=[]
             for m in goodMatch:
-                tp.append(trainKP[m.trainIdx].pt)
+                tp.append(self.trainKP[m.trainIdx].pt)
                 qp.append(queryKP[m.queryIdx].pt)
             tp,qp=np.float32((tp,qp))
             #os.system("say wow look a stopsign")
@@ -50,7 +51,7 @@ class FLANN(Process):
 
             trainBorder=np.float32([[[0,0],[0,self.height-1],[self.width-1,self.height-1],[self.width-1,0]]])
             queryBorder=cv2.perspectiveTransform(trainBorder,H)
-            os.system("say found")
+            #os.system("say found")
 
             if self.output_queue.full():
                 self.output_queue.get_nowait()
@@ -95,44 +96,45 @@ if __name__ == '__main__':
     threaded_mode = True
 
     process_list = []
-    Input_Queue = Queue(maxsize = 5)
-    Output_Queue = Queue(maxsize = 5)
-
+    Input_Queue = Queue(maxsize = 3)
+    Output_Queue = Queue(maxsize = 3)
     for x in range((threadn -1)):
-        ft = FLANN(frame_queue = Input_Queue, output_queue = Output_Queue, descriptor = trainDesc, height = h, width = w)
+        ft = FLANN(frame_queue = Input_Queue, output_queue = Output_Queue, descriptor = trainDesc, trainKP = trainKP, height = h, width = w)
         ft.daemon = True
         ft.start()
         process_list.append(ft)
 
     ch = cv2.waitKey(1)
     cv2.namedWindow('Threaded Video', cv2.WINDOW_NORMAL)
+
+    target = None
+
     while True:
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         put_frame(gray)
-        #resize
-        h,w = gray.shape
-        if(h > 720):
-            gray = cv2.resize(gray, (0,0), fx=720/h, fy=720/h)
-            h,w = gray.shape
-        l = int((w-h)/2)
-        r = h+l
-        #print(w, h, l, r)
-        gray = gray[0:h, l:r] #crop
-
         contour = cv2.Canny(gray, 80, 200, apertureSize = 3)
         combined = cv2.addWeighted(contour, 0.8, gray, 0.2, 0)
         combined = cv2.cvtColor(combined, cv2.COLOR_GRAY2BGR)
         if not Output_Queue.empty():
             result = Output_Queue.get()
+            target = result
             cv2.polylines(combined,[np.int32(result)],True,(0,0,255),5)
-            ch = cv2.waitKey(5)
+        else:
+            if target is not None:
+                cv2.polylines(combined,[np.int32(target)],True,(0,0,255),5)
+                target = None
 
 
-        stream = np.concatenate((combined, combined), axis=1)
+        h,w,c = combined.shape
         target = 400
-        # if(h > target):
-        #     stream = cv2.resize(stream, (0,0), fx=target/h, fy=target/h)
+        if(h > target):
+            combined = cv2.resize(combined, (0,0), fx=target/h, fy=target/h)
+            h,w,c = combined.shape
+        l = int((w-h)/2)
+        r = h+l
+        combined = combined[0:h, l:r] #crop
+        stream = np.concatenate((combined, combined), axis=1)
         cv2.imshow('frame',stream)
 
 
