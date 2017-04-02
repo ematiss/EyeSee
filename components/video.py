8c005f37bed163111d38bad56f5a8e63bf412cb1
Emils Matiss <emils.matiss@gmail.ca>
2017-04-02 04:32:09 -0400

    cleaned up folders

--- components/video.py
+++ components/video.py
@@ -10,10 +10,10 @@ while(True):
     #print(gray.shape)
     gray = gray[0:720, 280:1000]
     contour = cv2.Canny(gray, 80, 200)
-    
-    
-    combinedL = cv2.addWeighted(contour, 0.8, gray, 0.2, 0)
-    
+
+
+    combined = cv2.addWeighted(contour, 0.8, gray, 0.2, 0)
+
     stream = np.concatenate((combined, combined), axis=1)
     cv2.imshow('frame',stream)
     if cv2.waitKey(1) & 0xFF == ord('q'):
